339d940cdc5bf536c99680d3a710261812972d26
Chris Cowan <agentme49@gmail.com>
2016-12-15 01:18:54 -0800

    Make links work.

--- app/index.js
+++ app/index.js
@@ -1,10 +1,20 @@
 'use strict';
 (function() {
+  const {shell} = require('electron');
   const Kefir = require('kefir');
+  const url = require('url');
   const electronContextMenu = require('electron-context-menu');
 
   document.addEventListener('DOMContentLoaded', () => {
     const webview = document.getElementById('discord');
+
+    webview.addEventListener('new-window', e => {
+      const {protocol} = url.parse(e.url);
+      if (protocol === 'http:' || protocol === 'https:') {
+        shell.openExternal(e.url);
+      }
+    });
+
     Kefir.fromEvents(webview, 'dom-ready')
       .take(1)
       .onValue(() => {
